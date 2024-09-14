6579969847a647315ccf8fc1ba80374f546b110c
Pablo Romero <pablo_5_3_503@hotmail.com>
2024-09-13 22:26:30 -0300

    Se crearon las plantillas necesarias de pug, tambien se creo los el webRouter para la renderizacion de los mismismos

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
+const router = require("./routers/webRouter");
 
 const app = express();
 const publicStatic = path.join(__dirname, "/public");
@@ -10,10 +11,7 @@ app.set("view engine", "pug");
 
 app.use(morgan("dev"));
 app.use(express.static(publicStatic));
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use(router);
 
 app.listen(3000, () => {
   console.log("Tamo en linea perrro");
