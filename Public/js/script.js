409f9e4f3f754c460e4cdf99efaad9a5f9f3c58d
Pablo Romero <pablo_5_3_503@hotmail.com>
2024-09-13 21:27:03 -0300

    Se conecto los archivos estaticos que se tienen en el proyecto a la plantilla pug

---
+++ Public/js/script.js
@@ -0,0 +1,1 @@
+console.log("VAMO QUE TA CONECTADO TODO ");
--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+
 const app = express();
+const publicStatic = path.join(__dirname, "/public");
 
 app.set("views", "./view");
 app.set("view engine", "pug");
+
+app.use(morgan("dev"));
+app.use(express.static(publicStatic));
 
 app.get("/", (req, res) => {
   res.render("index");
